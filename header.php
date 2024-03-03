908a53a99549c3ec0d329c7b4093319738735eb2
plastikSun <marcom2k01@gmail.com>
2024-03-03 10:22:32 +0100

    add: bootstrap component navbar

--- header.php
+++ header.php
@@ -28,7 +28,9 @@
     }
     ?>
 
-
-    <header>
-        This is a header.
-    </header>
+    <div id="page" class="site">
+        <header id="masthead" class="site-header" role="banner">
+            <?php get_template_part('template-parts/header/nav') ?>
+        </header>
+        <div id="content" class="site-content"></div>
+    </div>
