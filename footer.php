4977ef67a99f9aa7c3e7774504de23a12f3733a8
plastikSun <marcom2k01@gmail.com>
2024-03-03 08:14:57 +0100

    create:c functions, header and footer files

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Footer template.
+ * 
+ * @package Mazzeo
+ */
+
+?>
+
+<footer>
+    This is a footer.
+</footer>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Header template.
+ * 
+ * @package Mazzeo
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MZZ Ingegneria</title>
+</head>
+
+<body>
+    <header>
+        This is a header.
+    </header>
