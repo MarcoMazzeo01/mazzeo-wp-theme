ed51112792a62ad8e1c5ec879c089d59368fa0c8
plastikSun <marcom2k01@gmail.com>
2024-03-03 08:53:56 +0100

    add: style enqueue function

--- functions.php
+++ functions.php
@@ -5,3 +5,12 @@
  * 
  * @package Mazzeo
  */
+
+function mazzeo_enqueue_scripts()
+{
+    wp_enqueue_style('stylesheet', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
+    // filemtime() returns a timestamp (unix) of the last edit of the given file passed as param, in this case the style.css file; 
+    //this is to prevent having to hard-refresh during development if the caches save old versions of the project.
+};
+
+add_action('wp_enqueue_scripts', 'mazzeo_enqueue_scripts');
