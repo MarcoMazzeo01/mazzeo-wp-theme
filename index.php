c2410d672de640d300655ec7e0ae5982d3447431
plastikSun <marcom2k01@gmail.com>
2024-03-03 08:15:10 +0100

    add: include footer and header files

--- index.php
+++ index.php
@@ -6,19 +6,12 @@
  * @package Mazzeo
  */
 
+include_once 'header.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MZZ Ingegneria</title>
-</head>
+<div class="content">
+    This is a content.
+</div>
 
-<body>
-
-</body>
-
-</html>
+<?php include_once 'footer.php' ?>
