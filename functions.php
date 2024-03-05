7a2dfb83cd2de3fe5b906ae42a57c101920457ff
plastikSun <marcom2k01@gmail.com>
2024-03-05 08:56:00 +0100

    add: comment

--- functions.php
+++ functions.php
@@ -6,11 +6,19 @@
  * @package Mazzeo
  */
 
+
 if (!defined('MAZZEO_DIR_PATH')) {
     define('MAZZEO_DIR_PATH', untrailingslashit(get_template_directory()));
 }
 
 require_once MAZZEO_DIR_PATH . '/inc/helpers/autoloader.php';
+
+function mazzeo_get_theme_instance()
+{
+    \MAZZEO_THEME\Inc\MAZZEO_THEME::get_instance(); //create theme class
+}
+
+mazzeo_get_theme_instance();
 
 
 function mazzeo_enqueue_scripts()
