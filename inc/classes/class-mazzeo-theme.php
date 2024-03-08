71cea839f155a8590c8120ab51e3f5e01be9c7dd
plastikSun <marcom2k01@gmail.com>
2024-03-08 07:53:36 +0100

    add: style and script enqueue

--- inc/classes/class-mazzeo-theme.php
+++ inc/classes/class-mazzeo-theme.php
@@ -18,6 +18,7 @@ class MAZZEO_THEME
     protected function __construct()
     {
         //load class
+
         $this->setup_hooks();
     }
 
@@ -31,7 +32,7 @@ class MAZZEO_THEME
     public function register_styles()
     {
         //register style.css
-        wp_register_style('style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
+        wp_register_style('style-css', get_stylesheet_uri(), [], filemtime(MAZZEO_DIR_PATH . '/style.css'), 'all');
         // filemtime() returns a timestamp (unix) of the last edit of the given file passed as param, in this case the style.css file; 
         //this is to prevent having to hard-refresh during development if the caches save old versions of the project.
 
@@ -45,7 +46,7 @@ class MAZZEO_THEME
     public function register_scripts()
     {
         //register script.js
-        wp_register_script('main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true);
+        wp_register_script('main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime(MAZZEO_DIR_PATH . '/assets/main.js'), true);
 
         //register bootstrap js
         wp_register_script('bootstrap-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.bundle.min.js', ['jquery'], false, true);
