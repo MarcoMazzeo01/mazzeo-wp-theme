852d10c88befad426a57940853e2720a18609c79
plastikSun <marcom2k01@gmail.com>
2024-03-09 21:09:38 +0100

    add: setup_theme(), theme support for title tag, custom logo and custom background

--- inc/classes/class-mazzeo-theme.php
+++ inc/classes/class-mazzeo-theme.php
@@ -18,13 +18,30 @@ class MAZZEO_THEME
     protected function __construct()
     {
         //load class
-        Assets::get_instance();
+        Assets::get_instance(); //enqueues style and scripts;
         $this->setup_hooks();
     }
 
     protected function setup_hooks()
     {
         //actions
+        add_action('after_setup_theme', [$this, 'setup_theme']);
+    }
 
+    public function setup_theme()
+    {
+        add_theme_support('title-tag');
+        add_theme_support('custom-logo', [
+            'header-text' => ['site-title', 'site-description'],
+            'height' => 25,
+            'width' => 25,
+            'flex-height' => false,
+            'flex-width' => false,
+        ]);
+
+        add_theme_support('custom-background', [
+            'default-image' => '',
+            'default-color' => '#fff',
+        ]);
     }
 }
