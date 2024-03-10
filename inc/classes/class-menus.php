2d00d3794c34363c1168ab495bcb3ff9bfa15daa
plastikSun <marcom2k01@gmail.com>
2024-03-10 08:11:24 +0100

    create: menu class + add: registered header and footer menus

---
+++ inc/classes/class-menus.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Register menus
+ * 
+ * @package Mazzeo
+ */
+
+namespace MAZZEO_THEME\Inc;
+
+use MAZZEO_THEME\Inc\Traits\Singleton;
+
+class Menus
+{
+    use Singleton;
+
+    protected function __construct()
+    {
+        //load class
+
+        $this->setup_hooks();
+    }
+
+    protected function setup_hooks()
+    {
+        //actions
+        add_action('init', [$this, 'register_menus']);
+    }
+
+    public function register_menus()
+    {
+        register_nav_menus([
+            'mazzeo-header-menu' => esc_html__('Header Menu', 'mazzeo'), //second arg is text domain in style.css
+            'mazzeo-footer-menu' => esc_html__('Footer Menu', 'mazzeo')
+        ]);
+    }
+}
