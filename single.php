a53aa7d9ad256f8e3f6c700acdc253e57c09dd7d
plastikSun <marcom2k01@gmail.com>
2024-03-23 17:04:17 +0100

    create: post template page

---
+++ single.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Main template file.
+ * 
+ * @package Mazzeo
+ */
+
+get_header()
+?>
+
+
+<div class="content">
+
+
+    <div class="container">
+        <?php
+
+        esc_html_e('Single post', 'mazzeo');
+        ?>
+    </div>
+</div>
+
+<?php get_footer();
