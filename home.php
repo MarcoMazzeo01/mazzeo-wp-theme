374ccb7fb0a6db00a69f06c593a16c84cd9785c9
plastikSun <marcom2k01@gmail.com>
2024-03-23 17:13:43 +0100

    add: the for loop to render posts list

--- home.php
+++ home.php
@@ -1,2 +1,37 @@
 <?php
-echo 'home/blog??';
+
+/**
+ * Blog list template file.
+ * 
+ * @package Mazzeo
+ */
+
+get_header()
+?>
+
+
+<div id="primary">
+    <main id="main" class="site-main mt-5" role="main">
+
+        <?php
+        if (have_posts()) {
+        ?>
+            <div class="container">
+                <?php
+                while (have_posts()) : the_post();
+                    the_title();
+                    the_excerpt();
+
+                endwhile;
+                ?>
+            </div>
+
+        <?php
+
+        }
+
+        ?>
+    </main>
+</div>
+
+<?php get_footer();
